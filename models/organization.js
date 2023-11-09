c6a57b9de6137aaedc3a595277f3414f63561fd1
MichaelFami <michaelfami@gmail.com>
2023-11-09 08:47:55 -0600

    more events

--- models/organization.js
+++ models/organization.js
@@ -3,7 +3,7 @@ const organizationSchema = new Schema (
    {
       name: { type: String, required: true },
       admin: { type: Schema.Types.ObjectId, ref: 'User' },
-      members:{ type: [Schema.Types.ObjectId], ref: 'User', default:undefined },
+      // members:{ type: [Schema.Types.ObjectId], ref: 'User', default:undefined },
       events: { type: [Schema.Types.ObjectId], ref: 'Event', default:undefined },
    },
 
