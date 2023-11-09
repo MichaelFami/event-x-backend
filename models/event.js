3b2e27b6d12470703ca6d0685cb34cb65d4b20ba
MichaelFami <michaelfami@gmail.com>
2023-11-09 10:48:54 -0600

    userevent1

--- models/event.js
+++ models/event.js
@@ -9,6 +9,8 @@ const eventSchema = new Schema (
       location: { type: String, required: true },
       capacity: { type: String, required: true },
       date:{type: Date, required: true},
+      rsvp:{ type: [Schema.Types.ObjectId], ref: 'User', default:[] },
+
 
    },
 
--- seed/eventXDatabase.js
+++ seed/eventXDatabase.js
@@ -90,6 +90,7 @@ const users = [
         location: "3102 n first st Austin, TX",
         capacity: "345",
         date: new Date("2017-04-02T15:30:00Z"),
+        rsvp:[],
   
      },
         {
@@ -97,63 +98,72 @@ const users = [
             description: "Community members gather to donate non-perishable food items.",
             location: "124 Main St, Springfield, IL",
             capacity: 200,
-            date: new Date("2018-09-15T12:00:00Z")
+            date: new Date("2018-09-15T12:00:00Z"),
+            rsvp:[],
         },
         {
             name: "Blanket Blessings",
             description: "Participants contribute warm blankets for the homeless.",
             location: "500 Oak St, Denver, CO",
             capacity: 150,
-            date: new Date("2019-11-20T18:00:00Z")
+            date: new Date("2019-11-20T18:00:00Z"),
+            rsvp:[],
         },
         {
             name: "Toy Drive Extravaganza",
             description: "A festive event where toys are collected for underprivileged children.",
             location: "815 Pine Ave, San Francisco, CA",
             capacity: 300,
-            date: new Date("2020-12-10T14:00:00Z")
+            date: new Date("2020-12-10T14:00:00Z"),
+            rsvp:[],
         },
         {
             name: "Books for All",
             description: "Book lovers unite to donate books for a community library.",
             location: "230 Maple Dr, Boston, MA",
             capacity: 180,
-            date: new Date("2018-06-25T16:45:00Z")
+            date: new Date("2018-06-25T16:45:00Z"),
+            rsvp:[],
         },
         {
             name: "Clean Water Crusade",
             description: "Supporters come together to fundraise for clean water initiatives.",
             location: "42 Riverfront Ave, Seattle, WA",
             capacity: 250,
-            date: new Date("2019-08-05T13:15:00Z")
+            date: new Date("2019-08-05T13:15:00Z"),
+            rsvp:[],
         },
         {
             name: "Winter Warmth Drive",
             description: "Collecting winter clothing for those in need during the cold season.",
             location: "710 Pine St, Chicago, IL",
             capacity: 180,
-            date: new Date("2021-01-15T17:30:00Z")
+            date: new Date("2021-01-15T17:30:00Z"),
+            rsvp:[],
         },
         {
             name: "Green Earth Gathering",
             description: "Environmental enthusiasts contribute to tree-planting initiatives.",
             location: "123 Greenway Blvd, Portland, OR",
             capacity: 200,
-            date: new Date("2022-04-22T10:00:00Z")
+            date: new Date("2022-04-22T10:00:00Z"),
+            rsvp:[],
         },
         {
             name: "Health and Hygiene Fair",
             description: "Donations of hygiene products for underserved communities.",
             location: "890 Wellness Lane, Miami, FL",
             capacity: 150,
-            date: new Date("2020-07-08T14:30:00Z")
+            date: new Date("2020-07-08T14:30:00Z"),
+            rsvp:[],
         },
         {
             name: "Tech for All",
             description: "Gathering used electronics to be refurbished for educational purposes.",
             location: "76 Tech Hub Dr, Silicon Valley, CA",
             capacity: 300,
-            date: new Date("2021-10-12T11:00:00Z")
+            date: new Date("2021-10-12T11:00:00Z"),
+            rsvp:[],
         }
      ]
      
